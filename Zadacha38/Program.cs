81fba925269a6b306d0fd4ad91c3938a23e3dd2c
Arina M <apelsinka7.ru@yandex.ru>
2022-10-06 00:05:39 +0300

    решение задачи 38

---
+++ Zadacha38/Program.cs
@@ -0,0 +1,27 @@
+﻿double[] numbers = new double[5];
+
+  for (int i = 0; i < numbers.Length; i++ )
+  {
+    numbers[i] = new Random().Next(1, 10);
+    Console.Write(numbers[i] + " ");
+  }
+
+double max = numbers[0];
+double min = numbers[0];
+
+  for (int i = 1; i < numbers.Length; i++)
+  {
+    if (max < numbers[i])
+    {
+      max = numbers[i];
+    }
+        if (min > numbers[i])
+    {
+      min = numbers[i];
+    }
+  }
+
+double diff = max - min;
+Console.WriteLine();
+
+  Console.WriteLine($"Разница между между максимальным {max} и минимальным {min} элементов массива: {diff}");
