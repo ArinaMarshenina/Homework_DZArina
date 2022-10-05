e4b68bc5f0053f4edbd4f887cef6187845d3b449
Arina M <apelsinka7.ru@yandex.ru>
2022-10-05 23:54:31 +0300

    решение задачи 36

---
+++ Zadacha36/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[] numbers = new int[4];
+int RandomNumbers(int [] array, int arr1, int arr2)
+  {
+    int sumElements = 0;
+    for (int i = 0; i <array.Length; i++ ){
+    array[i] = new Random().Next(arr1,arr2);
+
+    Console.Write(array[i] + " ");
+      if (i % 2 != 0)
+      {
+        sumElements = sumElements + array[i];
+      }
+    }
+  return sumElements;
+  }
+
+int count = RandomNumbers(numbers, 1, 4);
+Console.WriteLine();
+
+Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {count}");
