d0537ba5ca577be8ea3eb4f1d84d23ca4d1b9288
Arina M <apelsinka7.ru@yandex.ru>
2022-10-08 23:04:46 +0300

    решение задачи 41

---
+++ Zadacha41/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Сколько чисел вы хотите написать: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] numbers = new int[m];
+
+void GetNumbers(int m)
+{
+for (int i = 0; i < m; i++)
+  {
+    Console.Write($"Введите {i+1} число: ");
+    numbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Counting(int[] numbers)
+{
+  int count = 0;
+  for (int i = 0; i < numbers.Length; i++)
+  {
+    if(numbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+GetNumbers(m);
+
+Console.WriteLine($"Чисел больше 0: {Counting(numbers)} ");
